2fb1fe56b1ae1b48bda00192ef908a320662528f
Thanh0512 <95989862+Thanh0512@users.noreply.github.com>
2025-11-18 22:19:06 +0700

    fix error

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./featuredProperties.css";
-import axios from "./axios";
+import axios from "../../axios";
 
 const FeaturedProperties = () => {
     const [data, setData] = useState([]);
