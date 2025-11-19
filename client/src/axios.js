03f6b77635c70ad9657dfa59f437e2a0a6f0e0a5
Thanh0512 <95989862+Thanh0512@users.noreply.github.com>
2025-11-19 15:43:41 +0700

    Add request interceptor for authorization token
    Added request interceptor to include authorization token.

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
 const instance = axios.create({
-    baseURL: `${API_BASE}/api`,
-    withCredentials: true,
+    baseURL: `${API_BASE}/api`
 });
+instance.interceptors.request.use(config => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, error => Promise.reject(error));
+
 export default instance;
