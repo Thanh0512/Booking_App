9560d29217a92fd6c635d208e65c2fd4faa225aa
Thanh0512 <95989862+Thanh0512@users.noreply.github.com>
2025-11-19 19:36:04 +0700

    Update axios.js

--- admin/src/axios.js
+++ admin/src/axios.js
@@ -1,14 +1,22 @@
 import axios from "axios";
+
 const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
 const instance = axios.create({
-    baseURL: `${API_BASE}/api`
+  baseURL: `${API_BASE}/api`
 });
-instance.interceptors.request.use(config => {
+
+instance.interceptors.request.use(
+  (config) => {
     const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+
+    if (token && !config.url.includes("/login") && !config.url.includes("/register")) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
-}, error => Promise.reject(error));
+  },
+  (error) => Promise.reject(error)
+);
 
 export default instance;
